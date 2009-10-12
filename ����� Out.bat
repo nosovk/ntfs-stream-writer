bf53d1d1c1cab280a3a55c472d4ba30b47138b06
nosovk <nosovk@localhost>
2009-10-12 17:58:22 +0300

    hg https://ntfs-stream-writer.googlecode.com/hg/ ntfs-stream-writer

--- ����� Out.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cd "%~dp0"
-ntfs_stream_writer.exe "films:hy" "arc.rar" > nul && ^
-rar.exe x -o+ -os "arc.rar" %fplace% && ^
-del arc.rar || echo Error loading info  && PAUSE ^
--- ����� in.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cd "%~dp0" && ^
-rar.exe a -rr10 -t -df -k -m3 -o+ -os "arc.rar" "work" && ^
-ntfs_stream_writer.exe "arc.rar" "films:hy" > nul && ^
-del arc.rar || echo Error saving info && PAUSE
